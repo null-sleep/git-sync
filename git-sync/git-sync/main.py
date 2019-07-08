a9f9a19c979e961c84b4ef70362e5009908d3979
Dhruv Jauhar <dhruvjhr@gmail.com>
2019-07-08 08:41:22 -0700

    Dump

--- git-sync/git-sync/main.py
+++ git-sync/git-sync/main.py
@@ -1,6 +1,8 @@
+import re
 import os
 import logging
-from distutils.util import strtobool
+import subprocess
+import click
 
 
 class GitSyncException(Exception):
@@ -10,31 +12,82 @@ class GitSyncException(Exception):
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-home = os.environ.get('HOME')
-repo = os.environ.get('GIT_SYNC_REPO')
-branch = os.environ.get('GIT_SYNC_BRANCH', 'master')
-rev = os.environ.get('GIT_SYNC_REV', 'HEAD')
-depth = int(os.environ.get('GIT_SYNC_DEPTH', 0))
-root = os.environ.get('GIT_SYNC_ROOT', os.path.join(home, 'git/'))
-dest = os.environ.get('GIT_SYNC_DEST', '')
-wait = os.environ.get('GIT_SYNC_WAIT', 0)
-sync_timeout = int(os.environ.get('GIT_SYNC_TIMEOUT', 120))
-one_time = bool(strtobool(os.environ.get('GIT_SYNC_ONE_TIME', False)))
-max_failures = int(os.environ.get('GIT_SYNC_MAX_SYNC_FAILURES', 0))
-chmod = int(os.environ.get('GIT_SYNC_PERMISSIONS'))
+# Hack to provide key using envvar
+SSH_KEY_ENV_VAR = 'GIT_SYNC_SSH_KEY'
+DEFAULT_SSH_PATH = '/etc/git-secret/ssh'
 
 username = os.environ.get('GIT_SYNC_USERNAME')
 password = os.environ.get('GIT_SYNC_PASSWORD')
 
-ssh = bool(strtobool(os.environ.get('GIT_SYNC_SSH', False)))
-ssh_file = os.environ.get('GIT_SSH_KEY_FILE', '/etc/git-secret/ssh')
-ssh_env = os.environ.get('GIT_SSH_ENV_VAR', 'GIT_SSH_KEY')
-# TODO Host Files
+#ssh = bool(strtobool(os.environ.get('GIT_SYNC_SSH', False)))
+#ssh_file = os.environ.get('GIT_SSH_KEY_FILE', '/etc/git-secret/ssh')
+#ssh_env = os.environ.get('GIT_SSH_ENV_VAR', 'GIT_SSH_KEY')
 
-# repo = os.environ.get('')
+# Valid git urls
+# git@github.com:jlowin/git-sync.git
+# https://github.com/jlowin/git-sync.git
+def get_ssh_url(repo):
+    match_ssh = re.match(r'git@github.com:(.+?)/(.+?)\.git', repo)
+    match_http = re.match(r'https://github.com/(.+?)/(.+?)\.git', repo)
+    if match_ssh is not None:
+        return repo
+    if match_http is not None:
+        user = match_http.group(1)
+        project = match_http.group(2)
+        return f'git@github.com:{user}/{project}.git'
 
-def git_sync():
-    if repo is None:
-        raise GitSyncException('$GIT_SYNC_REPO must be provided')
-    if ssh:
-        setup_git_ssh(ssh_file)
+    raise GitSyncException(f'Invalid git repo url: {repo}')
+
+
+# permission expects an octal
+def create_ssh_file(ssh_key, path, permission=0o600):
+    logger.info('Setting up ssh key file')
+    try:
+        with open(path, 'w+') as fh:
+            fh.write(ssh_key)
+        os.chmod(path, permission)
+        logger.debug('Created ssh key file at: {path} with permission')
+    except Exception as e:
+        raise GitSyncException('Unable to create ssh key file') from e
+
+
+def shell():
+    pass
+
+
+@click.command()
+@click.option('--repo', envvar='GIT_SYNC_REPO', required=True)
+@click.option('--branch', envvar='GIT_SYNC_BRANCH', default='master')
+@click.option('--rev', envvar='GIT_SYNC_REV', default='HEAD')
+@click.option('--depth', envvar='GIT_SYNC_DEPTH', type=int, default=0)
+@click.option('--root', envvar='GIT_SYNC_ROOT')
+@click.option('--dest', envvar='GIT_SYNC_DEST', default='')
+@click.option('--wait', envvar='GIT_SYNC_WAIT', default=60)
+@click.option('--one-time', envvar='GIT_SYNC_RUN_ONCE', type=bool, default=False)
+
+def git_sync(repo, branch, rev, depth, root, dest, wait, one_time):
+    home = os.path.expanduser('~')
+    ssh_repo_url = get_ssh_url(repo)
+
+    # print(f"{repo}, {branch}, {rev}, {depth}, {root}, {dest}, {wait}, {one_time}, {ssh_repo_url}")
+    subprocess.run(['ls', '--all'], check=True, text=True)
+    exit()
+
+    if root is None:
+        root = os.path.join(home, 'git/')
+
+    dest = dest.rstrip('/')
+    if len(dest.split('/')) > 1:
+        raise GitSyncException('Destination must be a name')
+    dest = os.path.join(root, dest)
+
+    ssh_key = os.getenv(SSH_KEY_ENV_VAR)
+    if ssh_key is None:
+        raise GitSyncException('Missing environment variable: {SSH_KEY_ENV_VAR}')
+    create_ssh_file(ssh_key, DEFAULT_SSH_PATH)
+
+    #sync_repo(repo, branch, rev, depth, root, dest, DEFAULT_SSH_PATH)
+
+
+if __name__ == '__main__':
+    git_sync()
